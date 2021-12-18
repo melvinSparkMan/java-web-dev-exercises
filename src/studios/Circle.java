b4c2121799c0315096d3181acddf6a9641fc7ab1
melvinSparkMan <melvinsparkman@hotmail.com>
2021-12-18 15:45:34 -0600

    Committing the Java Web Dev: Data Types: Studios.

---
+++ src/studios/Circle.java
@@ -0,0 +1,8 @@
+package studios;
+
+public class Circle {
+
+    public static Double getArea(Double radius) {
+        return 3.14 * radius * radius;
+    }
+}
---
+++ src/studios/areaofacircle.java
@@ -0,0 +1,21 @@
+package studios;
+
+import org.launchcode.java.demos.lsn1datatypes.Message;
+
+import java.util.Scanner;
+
+public class areaofacircle {
+    public static void main(String[] args) {
+        double radius;
+        double area;
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.print("\nEnter a Radius: ");
+        radius = input.nextDouble();
+        area = Circle.getArea(radius);
+        input.close();
+
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+    }
+}
